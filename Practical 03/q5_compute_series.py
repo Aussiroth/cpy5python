46e46f509bba38599af11dbeda1e34e60f971c71
Alvin Yan <yan.hongyao.alvin@dhs.sg>
2013-02-21 20:13:52 +0800

    Modified Practical 3 Q4/5

--- Practical 03/q5_compute_series.py
+++ Practical 03/q5_compute_series.py
@@ -5,15 +5,13 @@
 #Description: Computes a series
 
 def series(i):
-    print ("i", "m(i)")
+    print ("i", "   ", "m(i)")
     sum=0.0
     k=1
     while k<=i:
         sum=sum+float(1/(2*k-1))
         sum=sum-float(1/(2*k+1))
-        print (k, "{0:.12f}".format(sum*4.0))
+        print (k, " "*(4-len(str(k))), "{0:>.11f}".format(sum*4.0))
         k=k+2
 
-
-i = int(input("Enter an odd integer: "))
-series(i)
+series(19)
